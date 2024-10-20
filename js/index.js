c14144c539dd36d30a89d7b4ecd0fd27ab773086
unknown <isxm55203@gmail.com>
2024-10-21 02:49:16 +0500

    hammasi tugadi +

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const burgerBtn = document.getElementById("burger-btn");
+const burgerClose = document.getElementById("burger-open-btn");
+const navModal = document.getElementById("nav-modal");
+const overlay = document.querySelector(".nav__overlay");
+
+burgerBtn.addEventListener("click", () => {
+  navModal.classList.toggle("nav__modal--active");
+  overlay.classList.toggle("nav__overlay--active");
+});
+
+overlay.addEventListener("click", () => {
+  navModal.classList.remove("nav__modal--active");
+  overlay.classList.remove("nav__overlay--active");
+});
+
+burgerClose.addEventListener("click", () => {
+  navModal.classList.remove("nav__modal--active");
+  overlay.classList.remove("nav__overlay--active");
+});
